ef0e37d32424b4f6c77284e693cd4c07ca6acbb7
Juani Forte <juani8_80@hotmail.com>
2017-04-26 21:55:09 -0300

    Trabajo tp4

---
+++ src/EntregaTp4/Main.java
@@ -0,0 +1,23 @@
+package EntregaTp4;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ArrayList<Elemento> objetos = new ArrayList<Elemento>();
+		ArrayList<Elemento> mochila = new ArrayList<Elemento>();
+
+		Elemento elemento1 = new Elemento(0.10,200,10);
+		Elemento elemento2 = new Elemento(1,100,2);
+
+		objetos.add(elemento1);
+		objetos.add(elemento2);
+
+		Greedy greedy = new Greedy(objetos, 1);
+
+		mochila = greedy.cargar();
+		System.out.println(mochila);
+	}
+}
